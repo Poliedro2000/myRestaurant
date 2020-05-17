53406e5edaf1af188b481d75b24dddb2481b9934
frank <francisco@getxplor.com>
2020-05-17 03:18:18 -0500

    Deleted route

--- routes/waiter-route/r-waiter.js
+++ routes/waiter-route/r-waiter.js
@@ -13,7 +13,6 @@ rWaiter.route('/waiter-create/', decode.authMethod).post(WaiterControll.createWa
 rWaiter.route('/waiter-list/', decode.authMethod).get(WaiterControll.listAllWaiter);
 rWaiter.route('/waiter-view/:_id', decode.authMethod).get(WaiterControll.viewWaiterDetail);
 rWaiter.route('/waiter-access/').post(WaiterControll.accessWaiter);
-rWaiter.route('/waiter-upd-img/:_id', decode.authMethod).put(WaiterControll.uploadImageWaiter);
 rWaiter.route('/waiter-upinf/:_id', decode.authMethod).put(WaiterControll.updateInfoWaiter);
 
 module.exports = rWaiter;
